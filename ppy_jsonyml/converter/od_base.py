87f04507f7097014f099edde0775c6ceae0b93be
hmtmcse <hmtm.cse@gmail.com>
2023-10-07 11:54:39 +0600

    Working on object dictionary converter

---
+++ ppy_jsonyml/converter/od_base.py
@@ -0,0 +1,10 @@
+class ODBase:
+    pass
+
+
+class ODAttributeDetails:
+    attributeName: str
+    typeName: str
+    objectType: str = None
+    dictKeyType: str = None
+    objectClass: any = None
